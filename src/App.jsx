b9346b527a15cc73ed35372178194f500b93d460
Reyhand <mohamad.reyhand.f@students.esqbs.ac.id>
2023-11-23 20:02:09 +0700

    Revert "fixing routing in navbar and page"
    This reverts commit da33da71024634349f5eceb7d35f3050ee689992.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-import React, { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-
-const Home = lazy(() => import('./pages/Home/Home'));
-const About = lazy(() => import('./pages/About/About'));
-const TesMental = lazy(() => import('./pages/TesMental/TesMental'));
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
     return (
         <Router>
-            <Suspense fallback={<div>Loading...</div>}>
+            <div>
                 <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/testmental" element={<TesMental />} />
-                </Routes>
-            </Suspense>
+            </div>
         </Router>
     );
 };
--- src/pages/TesMental/TesMental.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TesMental = () => {
-  return (
-    <div>TesMental</div>
-  )
-}
-
-export default TesMental
