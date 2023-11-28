bb257de182f7d3dd49121fae5ce9577f7bdda7ea
Inosensius G. Atok <inosensius06@gmail.com>
2023-11-28 19:44:57 +0700

    Memperbaiki resposive halaman kecemasan

--- src/pages/TestMental/kecemasanPage/hero.jsx
+++ src/pages/TestMental/kecemasanPage/hero.jsx
@@ -4,9 +4,9 @@ import hero from "../../../assets/cemas-1.png";
 
 const Hero = () => {
     return (
-            <div className='grid grid-cols-2 gap-px px-10 py-13 justify-between'>
+            <div className='min-h-[70vh] flex flex-col md:flex-row md:justify-between items-center md:mx-32 mx-5 mt-10'>
               <div className='basis-2/5'>
-                <img srcSet={hero} alt="hero" className='scale-100' />
+                <img srcSet={hero} alt="hero" className='flex flex-col items-stretch ml-5 max-lg:w-full  max-md:ml-0 ' />
               </div>
               <div className='basis-1/2 pt-10 max-w-lg'>
                 <h2 className='text-2xl font-black py-6'>Apa itu Kecemasan ?</h2>
