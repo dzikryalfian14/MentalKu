5dc090e4be4e81010b8b8cc667ec0da78a88856c
Inosensius G. Atok <inosensius06@gmail.com>
2023-12-07 00:48:51 +0700

    Memperbaiki resposifitas menjadi lebih baik lagi

--- src/pages/TestMental/Kategori/depresiPage/Penyebab.jsx
+++ src/pages/TestMental/Kategori/depresiPage/Penyebab.jsx
@@ -2,23 +2,25 @@ import React from "react"
 
 const Penyebab = () => {
     return (
-      <div className="mt-5">
-        <h1 className="text-2xl font-extrabold mt-10 mb-10 text-center">Penyebab Kecemasan</h1>
-        <div className='flex flex-wrap justify-center gap-14 px-10'>
-          <div className="grid justify-center gap-y-5">
-            <p className="bg-[#CDFAD5] text-center p-2 rounded-lg">Stres Berkepanjangan</p>
-            <p className="bg-[#CDFAD5] text-center p-2 rounded-lg">Ketidakmampuan Mengelola Emosi</p>
-            <p className="bg-[#CDFAD5] text-center p-2 rounded-lg">Kebiasaan Hidup dan Gaya Hidup</p>
-            <p className="bg-[#CDFAD5] text-center p-2 rounded-lg">Kondisi Lingkungan</p>
-          </div>
-          <div className="grid justify-center gap-y-5">
-            <p className="bg-[#CDFAD5] text-center p-2 rounded-lg">Kondisi Kesehatan fisik dan penyakit Lainnya</p>
-            <p className="bg-[#CDFAD5] text-center p-2 rounded-lg">Perubahan Kimia Otak</p>
-            <p className="bg-[#CDFAD5] text-center p-2 rounded-lg">Faktor Genetik dan Keturunan</p>
-            <p className="bg-[#CDFAD5] text-center p-2 rounded-lg">Perubahan Hidup yang Besar</p>
-          </div>
-        </div>
-      </div>
+      <div className="my-36">
+  <h1 className="text-2xl font-extrabold mt-10 mb-10 text-center px-8">Penyebab Depresi</h1>
+  <div className='flex flex-wrap justify-center gap-14 px-10 max-lg:gap-5 '>
+    <div className="grid justify-start gap-y-5">
+      <p className="flex-shrink-0 bg-[#FFF] text-start p-2 rounded-lg min-w-[300px] font-bold text-slate-400 border border-current sm:min-w-max md:w-[400px]">✔ Stres Berkepanjangan</p>
+      <p className="flex-shrink-0 bg-[#FFF] text-start p-2 rounded-lg min-w-[300px] font-bold text-slate-400 border border-current sm:min-w-max md:w-[400px]">✔ Ketidakmampuan Mengelola Emosi</p>
+      <p className="flex-shrink-0 bg-[#FFF] text-start p-2 rounded-lg min-w-[300px] font-bold text-slate-400 border border-current sm:min-w-max md:w-[400px]">✔ Kebiasaan Hidup dan Gaya Hidup</p>
+      <p className="flex-shrink-0 bg-[#FFF] text-start p-2 rounded-lg min-w-[300px] font-bold text-slate-400 border border-current sm:min-w-max md:w-[400px]">✔ Kondisi Lingkungan</p>
+    </div>
+    <div className="grid justify-center gap-y-5">
+      <p className="flex-shrink-0 bg-[#FFF] text-start p-2 rounded-lg min-w-[300px] font-bold text-slate-400 border border-current sm:min-w-max md:w-[400px]">✔ Kondisi Kesehatan fisik dan penyakit Lainnya</p>
+      <p className="flex-shrink-0 bg-[#FFF] text-start p-2 rounded-lg min-w-[300px] font-bold text-slate-400 border border-current sm:min-w-max md:w-[400px]">✔ Perubahan Kimia Otak</p>
+      <p className="flex-shrink-0 bg-[#FFF] text-start p-2 rounded-lg min-w-[300px] font-bold text-slate-400 border border-current sm:min-w-max md:w-[400px]">✔ Faktor Genetik dan Keturunan</p>
+      <p className="flex-shrink-0 bg-[#FFF] text-start p-2 rounded-lg min-w-[300px] font-bold text-slate-400 border border-current sm:min-w-max md:w-[400px]">✔ Perubahan Hidup yang Besar</p>
+    </div>
+  </div>
+</div>
+
+
     )
   }
   
--- src/pages/TestMental/Kategori/stressPage/Tes.jsx
+++ src/pages/TestMental/Kategori/stressPage/Tes.jsx
@@ -1,12 +1,19 @@
 import React from "react"
-import Button from "../../../../layout/Button"
+import { Link, useLocation } from "react-router-dom";
+
 
 const Tes = () => {
     return (
       <div className="p-16">
         <div className="text-center text-lg font-bold">Untuk mengetahui kondisi stress, silahkan ikuti tes!!</div>
         <div className="flex justify-center">
-          <Button title ="Tes Stress" />
+
+          
+        <Link to="/stressTest">
+          <button className="bg-[#cdcec8] py-2 px-5 rounded-lg mt-4 text-lg font-bold hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:bg-brightGreen hover:text-white transition-all">
+          Tes Stress
+        </button>
+      </Link>
         </div>
       </div>
     )
