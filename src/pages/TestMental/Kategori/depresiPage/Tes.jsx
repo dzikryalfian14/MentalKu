fd9ba4f5198658845cade2c44353997426b2c006
Inosensius G. Atok <inosensius06@gmail.com>
2023-12-12 15:59:03 +0700

    memperbaiki modal mulai test

--- src/pages/TestMental/Kategori/depresiPage/Tes.jsx
+++ src/pages/TestMental/Kategori/depresiPage/Tes.jsx
@@ -25,27 +25,32 @@ const Tes = () => {
         </button>
       </div>
 
-      {/* Modal */}
-      <Modal
+{/* Modal */}
+<Modal
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
-        className="modal p-4 bg-[#cdcec8] rounded-md max-w-md mx-auto mt-24"
-        overlayClassName="overlay fixed top-0 left-0 right-0 bottom-0"
+        className="modal p-4 bg-[#ffffff] rounded-md max-w-6xl mx-auto mt-24 max-h-80 border border-solid border-rose-600 overflow-auto px-10"
+        overlayClassName="overlay bg-white bg-opacity-70 fixed top-0 left-0 right-0 bottom-0"
       >
-        <div className="content text-center">
-          <p className="mb-4 text-lg">
-            Apakah Anda yakin ingin melakukan tes depresi?
-          </p>
+        <div className="content text-center relative">
+        <p className="text-red-500 text-2xl font-bold">Sebelum memulai, kami ingin memberikan beberapa informasi penting:</p>
+        <ul className=" list-decimal text-justify leading-loose pt-3 text-base sm:text-2xl xl:text-3xl">
+            <li>Hasil tes ini bukan pengganti konsultasi langsung dengan profesional kesehatan mental. Jika Anda memiliki kekhawatiran serius tentang kesehatan mental Anda, segera hubungi profesional kesehatan.</li>
+            <li>Pastikan Anda berada di lingkungan yang tenang dan nyaman selama tes. Temukan tempat yang bebas gangguan dan luangkan waktu yang cukup.</li>
+            <li>Tes ini dirancang untuk memberikan pemahaman umum tentang kesehatan mental Anda. Jawablah dengan jujur dan terbuka.</li>
+            <li>Apabila Anda merasa sangat kewalahan atau mengalami krisis mental, segera hubungi layanan darurat atau konsultasikan dengan profesional kesehatan mental.</li>
+            <li>Setelah tes selesai, kami akan menyediakan saran berdasarkan hasil Anda. Namun, evaluasi lebih mendalam dapat diperoleh melalui konsultasi dengan seorang profesional.</li>
+        </ul>
           <div className="flex justify-center">
             <button
               onClick={() => setModalIsOpen(false)}
-              className="bg-red-500 text-white py-2 px-4 mr-2 rounded-md"
+              className="bg-red-500  hover:bg-red-600 active:bg-red-700 focus:outline-none focus:ring focus:ring-red-300 text-white py-2 px-4 rounded-md  mr-2 absolute -top-5 -right-12"
             >
-              Tidak
+              X
             </button>
             <Link to="/tes-kesehatan-mental/depressiontest">
-              <button className="bg-green-500 text-white py-2 px-4 rounded-md">
-                Iya
+              <button className="bg-green-500 hover:bg-green-600 active:bg-green-700 focus:outline-none focus:ring focus:ring-green-300 text-white py-2 px-4 rounded-md">
+                Mulai Tes
               </button>
             </Link>
           </div>
